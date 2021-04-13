c07badfe1afb9f5d92fd396cb025e23a5d9ea49f
David Gustafsson <glasklas@gmail.com>
2021-04-13 17:41:09 +0200

    Move data directies out of repo and add config file

--- loadlibris.sh
+++ loadlibris.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
-base_dir="/home/koha/loadlibris"
-koha_user="koha"
-koha_shell_bin="$base_dir/koha-shell"
-#in_dir="$base_dir/in"
-in_dir="/home/koha/librload/in"
-in_archive_dir="$base_dir/in_archive"
-adjustlibris_err_dir="$base_dir/adjustlibris_err"
-adjustlibris_bin="/home/koha/adjustlibris/main.rb"
+script_dir="$(dirname "$(readlink -f "$0")")"
+source "$script_dir/loadlibris.sh.conf"
 
-bulkmarcimport_in_dir="$base_dir/bulkmarcimport_in"
-bulkmarcimport_err_dir="$base_dir/bulkmarcimport_err"
-bulkmarcimport_log_file="/var/log/bulkmarcimport"
+koha_shell_bin="$script_dir/koha-shell"
 
-koha_base_dir="/home/koha/koha-lab/current"
-koha_instance="koha"
-bulkmarcimport_bin="$koha_base_dir/misc/migration_tools/bulkmarcimport.pl"
-bulkmarcimport_to_marc_plugin="Koha::Plugin::Se::Ub::Gu::MarcImport"
-bulkmarcimport_match="System-control-number,035a"
+# TODO: Perhaps place these in loadlibris.sh.conf?
 
-done_dir="$base_dir/done"
 
 function log_error {
-  logger -s -p syslog.err "$1" 2>> /var/log/loadlibris
+  logger -s -p syslog.err "$1" 2>> "$loadlibris_log_dir"
 }
 
 function log_info {
-  logger -s -p syslog.info "$1" 2>> /var/log/loadlibris
+  logger -s -p syslog.info "$1" 2>> "$loadlibris_log_dir"
 }
 
 for filepath in $(find "$in_dir" -name '*.marc' | sort); do
   filename=$(basename "$filepath")
-  errors=$(sudo -u $koha_user bash -c "source \$HOME/.rvm/scripts/rvm && cd /home/koha/adjustlibris && ./main.rb \"$filepath\" \"$bulkmarcimport_in_dir/$filename\" 2>&1")
+  errors=$(sudo -u $koha_user bash -c "source \$HOME/.rvm/scripts/rvm && cd \$adjustlibris_path/adjustlibris && ./main.rb \"$filepath\" \"$bulkmarcimport_in_dir/$filename\" 2>&1")
   if [ $? -eq 0 ]; then
     log_info "ajustlibris successfully processed \"$filename\""
     mv "$filepath" "$in_archive_dir/"
@@ -42,6 +29,7 @@ done
 
 for filepath in $(find "$bulkmarcimport_in_dir" -name '*.marc' | sort); do
   filename=$(basename "$filepath")
+  #TODO: Errors are thrown away here? Probably for a reason
   errors=$($koha_shell_bin -c /home/koha/koha-lab/current/misc/migration_tools/bulkmarcimport.pl\ -b\ -file\ \"$filepath\"\ -insert\ -update\ -c\=MARC21\ -tomarcplugin\ \"Koha::Plugin::Se::Ub::Gu::MarcImport\" $koha_instance 2>&1 1>/dev/null)
   if [ $? -eq 0 ]; then
     log_info "bulkmarcimport successfully processed \"$filename\""
