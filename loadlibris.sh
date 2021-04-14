6a7005a2fe7414158c81bb9fcc222351046c53a0
David Gustafsson <glasklas@gmail.com>
2021-04-14 15:49:35 +0200

    Remove adjustlibris step since performed by plugin

--- loadlibris.sh
+++ loadlibris.sh
@@ -4,9 +4,6 @@ source "$script_dir/loadlibris.sh.conf"
 
 koha_shell_bin="$script_dir/koha-shell"
 
-# TODO: Perhaps place these in loadlibris.sh.conf?
-
-
 function log_error {
   logger -s -p syslog.err "$1" 2>> "$loadlibris_log_dir"
 }
@@ -16,15 +13,9 @@ function log_info {
 }
 
 for filepath in $(find "$in_dir" -name '*.marc' | sort); do
-  filename=$(basename "$filepath")
-  errors=$(sudo -u $koha_user bash -c "source \$HOME/.rvm/scripts/rvm && cd \$adjustlibris_path/adjustlibris && ./main.rb \"$filepath\" \"$bulkmarcimport_in_dir/$filename\" 2>&1")
-  if [ $? -eq 0 ]; then
-    log_info "ajustlibris successfully processed \"$filename\""
-    mv "$filepath" "$in_archive_dir/"
-  else
-    log_error "adjustlibris on file \"$adjustlibris_err_dir/$filename\" failed with exit status $? and output \"$errors\""
-    mv "$filepath" "$adjustlibris_err_dir/"
-  fi
+  # Archive all incoming files
+  cp "$filepath" "$in_archive_dir/"
+  mv "$filepath" "$bulkmarcimport_in_dir/"
 done
 
 for filepath in $(find "$bulkmarcimport_in_dir" -name '*.marc' | sort); do
