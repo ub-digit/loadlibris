737a02852af8ea6e561ba5e78dd9888cf643a41e
David Gustafsson <david.gustafsson@ub.gu.se>
2022-04-05 16:46:05 +0200

    Use hopefully less confusing config variable names

--- loadlibris.sh
+++ loadlibris.sh
@@ -12,7 +12,7 @@ function log_info {
   logger -s -p syslog.info "$1" 2>> "$loadlibris_log_dir/loadlibris"
 }
 
-for filepath in $(find "$in_dir" -name '*.marc' | sort); do
+for filepath in $(find "$fetchlibris_done_dir" -name '*.marc' | sort); do
   # Archive all incoming files
   cp "$filepath" "$bulkmarcimport_in_archive_dir/"
   mv "$filepath" "$bulkmarcimport_in_dir/"
@@ -24,7 +24,7 @@ for filepath in $(find "$bulkmarcimport_in_dir" -name '*.marc' | sort); do
   errors=$($koha_shell_bin -c /home/koha/koha-lab/current/misc/migration_tools/bulkmarcimport.pl\ -b\ -file\ \"$filepath\"\ -record_id_match\ -insert\ -update\ -c\=MARC21\ -tomarcplugin\ \"Koha::Plugin::Se::Ub::Gu::MarcImport\" $koha_instance 2>&1 1>/dev/null)
   if [ $? -eq 0 ]; then
     log_info "bulkmarcimport successfully processed \"$filename\""
-    mv "$filepath" "$done_dir/"
+    mv "$filepath" "$bulkmarcimport_done_dir/"
   else
     log_error "bulkmarcimport on file \"$bulkmarcimport_err_dir/$filename\" failed with exit status $?"
     mv "$filepath" "$bulkmarcimport_err_dir/$filename"
