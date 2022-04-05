ca6ae565a70e45117694bb57b21f3677f5dd0ac7
David Gustafsson <david.gustafsson@ub.gu.se>
2022-04-05 17:24:21 +0200

    Add more logging

--- loadlibris.sh
+++ loadlibris.sh
@@ -20,14 +20,14 @@ done
 
 for filepath in $(find "$bulkmarcimport_in_dir" -name '*.marc' | sort); do
   filename=$(basename "$filepath")
-  #TODO: Errors are thrown away here? Probably for a reason
-  errors=$($koha_shell_bin -c $koha_path/misc/migration_tools/bulkmarcimport.pl\ -b\ -file\ \"$filepath\"\ -record_id_match\ -insert\ -update\ -c\=MARC21\ -tomarcplugin\ \"Koha::Plugin::Se::Ub::Gu::MarcImport\" $koha_instance 2>&1 1>/dev/null)
+  output=$($koha_shell_bin -c $koha_path/misc/migration_tools/bulkmarcimport.pl\ -b\ -file\ \"$filepath\"\ -l=\"$loadlibris_log_dir/bulkmarcimport.log\"\ -append\ -match_record_id\ -insert\ -update\ -c\=MARC21\ -tomarcplugin\ \"Koha::Plugin::Se::Ub::Gu::MarcImport\" $koha_instance 2>&1)
   if [ $? -eq 0 ]; then
     log_info "bulkmarcimport successfully processed \"$filename\""
+    echo "$output\n\n" >> "$loadlibris_log_dir/bulkmarcimport.output"
     mv "$filepath" "$bulkmarcimport_done_dir/"
   else
     log_error "bulkmarcimport on file \"$bulkmarcimport_err_dir/$filename\" failed with exit status $?"
     mv "$filepath" "$bulkmarcimport_err_dir/$filename"
-    echo "$errors" > "$bulkmarcimport_err_dir/${filename}.err"
+    echo "$output" > "$bulkmarcimport_err_dir/${filename}.err"
   fi
 done
